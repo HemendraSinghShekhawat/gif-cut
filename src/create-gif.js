57fc1f528156c8df971624862a5205bca944ad1c
HemendraSinghShekhawat <hemustudys@gmail.com>
2025-09-02 22:29:54 +0530

    byte array implementation in for gif encoder

--- src/create-gif.js
+++ src/create-gif.js
@@ -1,10 +1,31 @@
 "use strict";
-console.log("Hello, World!");
-const gifBuffer = [];
-const writeUTFBytes = (str) => {
-    for (let len = str.length, i = 0; i < len; i++) {
-        gifBuffer.push(str.charCodeAt(i));
+class ByteArray {
+    bin = [];
+    constructor() {
+        this.bin = [];
     }
-};
-writeUTFBytes("GIF89a");
+    getChrCode(i) {
+        return String.fromCharCode(i);
+    }
+    getData() {
+        for (var v = "", l = this.bin.length, i = 0; i < l; i++)
+            v += this.getChrCode(this.bin[i]);
+        return v;
+    }
+    writeByte(val) {
+        this.bin.push(val);
+    }
+    writeBytes(array, offset, length) {
+        for (let l = length || array.length, i = offset || 0; i < l; i++) {
+            this.writeByte(array[i]);
+        }
+    }
+    writeUTFBytes(str) {
+        for (let len = str.length, i = 0; i < len; i++) {
+            this.bin.push(str.charCodeAt(i));
+        }
+    }
+}
+const gifBuffer = new ByteArray();
+gifBuffer.writeUTFBytes("GIF89a");
 console.log(gifBuffer);
--- src/create-gif.ts
+++ src/create-gif.ts
@@ -1,13 +1,45 @@
-// const gifData:  = []
-console.log("Hello, World!");
-
-const gifBuffer: number[] = [];
-
-const writeUTFBytes = (str: string) => {
-  for (let len = str.length, i = 0; i < len; i++) {
-    gifBuffer.push(str.charCodeAt(i));
+class ByteArray {
+  bin: number[] = [];
+  constructor() {
+    this.bin = [];
   }
-};
 
-writeUTFBytes("GIF89a");
+  getChrCode(i: number) {
+    return String.fromCharCode(i);
+    // return chr[i];
+  }
+
+  getData() {
+    for (var v = "", l = this.bin.length, i = 0; i < l; i++)
+      v += this.getChrCode(this.bin[i]);
+    return v;
+  }
+
+  writeByte(val: number) {
+    this.bin.push(val);
+  }
+
+  writeBytes(array: number[], offset?: number, length?: number) {
+    for (let l = length || array.length, i = offset || 0; i < l; i++) {
+      this.writeByte(array[i]);
+    }
+  }
+
+  writeUTFBytes(str: string) {
+    for (let len = str.length, i = 0; i < len; i++) {
+      this.bin.push(str.charCodeAt(i));
+    }
+  }
+}
+
+// class GIFEncoder {
+// 	width: number;
+// 	height: number;
+// 	transparent: null;
+//
+// }
+
+const gifBuffer = new ByteArray();
+
+gifBuffer.writeUTFBytes("GIF89a");
 console.log(gifBuffer);
